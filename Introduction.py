bd8bbbe63b16e4cbb651e76f08b2364d6f567ca5
Indra Deva Aji Zakaria <indra.d373@gmail.com>
2023-08-09 17:13:06 +0700

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -5,14 +5,6 @@ st.set_page_config(
     page_icon="👋",
 )
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-    #GithubIcon {
-      visibility: hidden;
-    }
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
 st.title('Klasifikasi Gaya Belajar Anak')
 st.text('Selamat di aplikasi web untuk klasifikasi persona anak menggunakan child-based\npersona framework.')
 st.text('Anda akan diminta untuk mengisi beberapa pertanyaan seputar kebutuhan, kemampuan\ndan pengalaman anak anda selama mengikuti kegiatan belajar mengajar baik \ndi rumah, di sekolah, atau di luar sekolah.')
