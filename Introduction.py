96e5b50642432da231f0310078475b0c2e417632
Indra Deva Aji Zakaria <indra.d373@gmail.com>
2023-08-09 23:01:02 +0700

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -5,15 +5,6 @@ st.set_page_config(
     page_icon="👋",
 )
 
-styl = f"""
-    <style>
-        .css-q16mip {
-        visibility: hidden;
-        }
-    </style>
-    """
-st.markdown(styl, unsafe_allow_html=True)
-
 st.title('Klasifikasi Gaya Belajar Anak')
 st.text('Selamat di aplikasi web untuk klasifikasi persona anak menggunakan child-based\npersona framework.')
 st.text('Anda akan diminta untuk mengisi beberapa pertanyaan seputar kebutuhan, kemampuan\ndan pengalaman anak anda selama mengikuti kegiatan belajar mengajar baik \ndi rumah, di sekolah, atau di luar sekolah.')
