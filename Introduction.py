206366a3a86d0e71e2bab34b5a964ebed066a9c5
Indra Deva Aji Zakaria <indra.d373@gmail.com>
2023-08-23 08:10:46 +0700

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -7,6 +7,6 @@ st.set_page_config(
 
 st.title('Classification of Child Learning Styles')
 st.text('Welcome to the web application for classifying child personas using the child-based\npersona framework.')
-st.text('You will be prompted to answer several questions regarding your child's needs, abilities, and \n experiences during learning activities, whether at home, school,\nor outside of school.')
+st.text('You will be prompted to answer several questions regarding your child needs, abilities, and \n experiences during learning activities, whether at home, school,\nor outside of school.')
 st.text("\n")
 
