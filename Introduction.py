90baf758e864180c957488de8edeb7684bf291f1
Indra Deva Aji Zakaria <indra.d373@gmail.com>
2023-08-23 07:50:04 +0700

    Update Introduction.py
    change to english

--- Introduction.py
+++ Introduction.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Klasifikasi Persona Anak",
+    page_title="Child Personas Classification",
     page_icon="👋",
 )
 
-st.title('Klasifikasi Gaya Belajar Anak')
-st.text('Selamat di aplikasi web untuk klasifikasi persona anak menggunakan child-based\npersona framework.')
-st.text('Anda akan diminta untuk mengisi beberapa pertanyaan seputar kebutuhan, kemampuan\ndan pengalaman anak anda selama mengikuti kegiatan belajar mengajar baik \ndi rumah, di sekolah, atau di luar sekolah.')
+st.title('Classification of Child Learning Styles')
+st.text('Welcome to the web application for classifying child personas using the child-based\npersona framework.')
+st.text('You will be prompted to answer several questions regarding your child's needs, abilities, and\nexperiences during learning activities, whether at home, school,\nor outside of school.')
 st.text("\n")
 
